a7fffe6f32009371007d2e3977da9b89e8bbbb54
Adria Castany Carranza <adriacastany@hotmail.es>
2022-04-28 13:52:42 +0200

    si-mon

--- app/src/main/java/com/example/minimochis/simon.java
+++ app/src/main/java/com/example/minimochis/simon.java
@@ -1,14 +1,21 @@
 package com.example.minimochis;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Random;
+
+import pl.droidsonroids.gif.GifImageView;
 
 public class simon  extends AppCompatActivity {
     //Interfaz
@@ -17,9 +24,14 @@ public class simon  extends AppCompatActivity {
     private Button tres;
     private Button cuatro;
     private TextView punts,nivell;
-    private FloatingActionButton
+
+    private FloatingActionButton volver, play;
+    private GifImageView minimochi;
+    private ImageView cartel;
 
     //Variables
+    int nums[];
+    int n= 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +42,73 @@ public class simon  extends AppCompatActivity {
         dos = findViewById(R.id.b2);
         tres = findViewById(R.id.b3);
         cuatro = findViewById(R.id.b4);
+
+        //gif i imatge
+        minimochi = findViewById(R.id.minimochi);
+        cartel = findViewById(R.id.cartel);
+
+        //Floats
+        volver = findViewById(R.id.volver);
+        play = findViewById(R.id.play);
+
         //Text
         punts = findViewById(R.id.punts);
         nivell = findViewById(R.id.nivell);
-        //recogida campos de texto
+
+        minimochi.setImageResource(R.drawable.minimochirosa);
+
+        volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(String.valueOf(Jocs.class));
+                startActivity(intent);
+            }
+        });
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newrandom();
+
+            }
+        });
+    }
+
+    public void newrandom(){
+        Random random = new Random(); int i1 = (random.nextInt(4) + 1);
+        nums[n] = random.nextInt();
+        n++;
+
+        for(int i=0; i==n;i++){
+            new CountDownTimer(3000, 1000) {
+
+                public void onTick(long millisUntilFinished) {
+                    if(nums[i] == 1){
+
+                    } else if (nums[i == 2){
+
+                    }
+                }
+
+                @Override
+                public void onFinish() {
+
+                }
+            }.start();
+        }
+    }
+
+    public void color(){
+        minimochi.setImageResource(R.drawable.minimochi_rosa_punch);
+        if (nums[n] == 1){
+            cartel.setImageResource(R.drawable.cartelred);
+        }else if (nums[n] == 2){
+            cartel.setImageResource(R.drawable.cartelblue);
+        }else if (nums[n] == 3){
+            cartel.setImageResource(R.drawable.cartelyellow);
+        }else if (nums[n] == 4){
+            cartel.setImageResource(R.drawable.cartelgreen);
+        }
     }
 
 
